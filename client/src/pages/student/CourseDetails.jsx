554c013eb98c6f0428f9a9a674fabbdeaabbbccd
Manikandan <nareshk9451@gmail.com>
2025-09-19 18:54:26 +0530

    update course details

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -46,7 +46,7 @@ const CourseDetails = () => {
         console.log(data);
       if(data.success){
       
-        window.location.replace(session_url);
+        window.location.replace(data.session_url);
       }else{
         toast.error(data.message)
       }
